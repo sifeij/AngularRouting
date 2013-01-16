055124c0549ba7ecc43641460410b8a685cd4bfa
sifeij <sophie.jiang@dial800.com>
2013-01-16 10:46:51 -0800

    fixed missing curly bracket and duplicated function name in app.js

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
 angular.module('enterprise', []).
-	config(function($routeProvider, $locationProvider){
-		$locationProvider.html5Mode(true); // to get rid of "#" in url
-
+	//config(function($routeProvider, $locationProvider){
+		//$locationProvider.html5Mode(true); // to get rid of "#" in url
+	config(['$routeProvider', function($routeProvider){
 		$routeProvider.
-			when("/", {templateUrl:"/partials/list.html"}).
-			when("/new", {templateUrl:"/partials/edit.html", controller:"NewCtrl"})
-			when("/edit/:id", {templateUrl:"/partials/edit.html", controller:"EditCtrl"}).
-			otherwise({redirectTo:"/"}); //handle the url that does not exist
-	})
+			when('/angularrouting/', {templateUrl: 'partials/list.html'});
+			//when('/angularrouting/new', {templateUrl: 'partials/edit.html', controller: NewCtrl});
+		//$routeProvider.when("/angularrouting/edit/:id", {templateUrl:"/partials/edit.html", controller:"EditCtrl"});
+		//$routeProvider.otherwise({redirectTo:"/"}); //handle the url that does not exist
+	}]);
 
-function NewCtrl ($scope, $location, $routeParams) {
+function EditCtrl ($scope, $location, $routeParams) {
 	$scope.person = $scope.crew[$routeParams.id];
 	$scope.save = function(){
 		$location.path("/");
+	}
 }
 
 function NewCtrl ($scope, $location) {
 	$scope.person = {name:"", description:""};
-	$scope.save = function(){
-		$scope.crew.push($scope.person);
-		$location.path("/");
-	}	
-}
-
-function EditCtrl ($scope, $location) {
-	$scope.person = {name:"", description:""}
 	$scope.save = function(){
 		$scope.crew.push($scope.person);
 		$location.path("/");
